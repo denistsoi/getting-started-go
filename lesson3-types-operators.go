6c370123c79880e480ecf2140eefa262d5c0516a
denistsoi <denistsoi@gmail.com>
2016-09-12 11:58:45 +0800

    ADD: Lesson3, types

---
+++ lesson3-types-operators.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Numbers: ")
+  fmt.Println("Addition ")
+  fmt.Println("1 + 1 =", 1 + 1)
+
+  fmt.Println("Subtraction ")
+  fmt.Println("2 - 1 =", 2 - 1)
+
+  fmt.Println("Multiplication ")
+  fmt.Println("3 * 3 =", 3 * 3)
+
+  fmt.Println("Division (float)")
+  fmt.Println("4 / 3 =", 4.0 / 3.0)
+
+  fmt.Println("Division (int)")
+  fmt.Println("4 / 3 =", 4 / 3)
+
+  fmt.Println("Remainder ")
+  fmt.Println("3 % 2 =", 3 % 2)
+}
